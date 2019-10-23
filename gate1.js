a233160b101ea9dcebb93cf8e553da5f8b085972
yaderson <39221701+yaderson@users.noreply.github.com>
2019-10-23 00:15:31 -0500

    update and add new function, validation and more

--- gate1.js
+++ gate1.js
@@ -1,7 +1,7 @@
 function proxyCheck(){
 
 
-var respo = true;
+let respo = true;
 
 
 return respo;
@@ -9,45 +9,44 @@ return respo;
 $(document).ready(function() {
 
 
-var li = 0;
-    var de = 0;
+let li = 0;
+var de = 0;
 
 
 
-    $('.start').click(async function(){
+$('.start').click(async function(){
     
-    var pro = $('#tar').val().split(/\n/);
-    var cantidad = pro.length;
+    let pro = $('#tar').val().split(/\n/);
+    let cantidad = pro.length;
     $('#prxi').text(cantidad);
     
     if(cantidad >= 1){
     
-    //window.alert(pro+' - '+cantidad);
-    for(var i = 0; i<cantidad;i++){
-    var ip = pro[i].split(':')[0];
-    var port = pro[i].split(':')[1];
-    
-        
-   //const proxy = { host: ip, port: port, proxytype: 'socks5' };
-   await axios.get('./checker.php?ip='+ip+'&port='+port+'&timeout=1')
-   .then(function(response){
-   
-   
-    if(response.data.result.success){
-    li++
-    $('#prxl').text(li);
-    $('#tx-live').append(response.data.result.proxy.ip+":"+response.data.result.proxy.port+"<br>");
-    
-    }else{
-    de++
-    $('#prxd').text(de);
-    $('#tx-dead').append(response.data.result.proxy.ip+":"+response.data.result.proxy.port+"<br>");
+        //window.alert(pro+' - '+cantidad);
+        for(var i = 0; i < cantidad && chkIsPrx(pro[i]);i++){
+        var ip = pro[i].split(':')[0];
+        var port = pro[i].split(':')[1];
+
+
+       //const proxy = { host: ip, port: port, proxytype: 'socks5' };
+       await axios.get('./checker.php?ip='+ip+'&port='+port+'&timeout=1')
+       .then(function(response){
+
+
+        if(response.data.result.success){
+            li++;
+            $('#prxl').text(li);
+            $('#tx-live').append(response.data.result.proxy.ip+":"+response.data.result.proxy.port+"<br>");
+        }else{
+            de++;
+            $('#prxd').text(de);
+            $('#tx-dead').append(response.data.result.proxy.ip+":"+response.data.result.proxy.port+"<br>");
     } 
     
     
     
     })
-   .catch(function(error){window.alert(error)}) 
+   .catch(function(error){window.alert(error); console.error(error); //For Debug :) }) 
     
     
    
@@ -55,11 +54,16 @@ var li = 0;
     } 
     
     }else{
-    window.alert(" Ingresa tus proxys prr >:v ");
-    
+        window.alert(" Ingresa tus proxys prr >:v ");
+        console.error("Need all input data")
     }
    
     
-    })
+    });
 });
 
+fuction chkIsPrx(prx){ //chk if input contain ip:port
+if(prx.split(":").length == 2){ 
+  return true
+}else{ return false}
+}
