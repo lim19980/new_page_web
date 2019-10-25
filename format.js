0adee90adf158c315dc0364b849b941fa1b8c9ee
yaderson <39221701+yaderson@users.noreply.github.com>
2019-10-24 23:11:06 -0500

    other validation function
    ...

--- format.js
+++ format.js
@@ -2,19 +2,19 @@ function getlist(){
 var sockslist=document.frm.sockslist.value;
 var fuck=sockslist.match(/\d{1,3}([.])\d{1,3}([.])\d{1,3}([.])\d{1,3}((:)|(\s)+)\d{1,8}/g);
 if(fuck){
-var list="";
-for(var i=0;i<fuck.length;i++){
+  var list="";
+  for(var i=0;i<fuck.length;i++){
 
-if(fuck[i].match(/\d{1,3}([.])\d{1,3}([.])\d{1,3}([.])\d{1,3}(\s)+\d{1,8}/g)){
-fuck[i]=fuck[i].replace(/(\s)+/,':');
+    if(fuck[i].match(/\d{1,3}([.])\d{1,3}([.])\d{1,3}([.])\d{1,3}(\s)+\d{1,8}/g)){
+    fuck[i]=fuck[i].replace(/(\s)+/,':');
 
+  }
+  list=list+fuck[i];
+  if(i!=(fuck.length-1)){
+  list=list+"\n";
+  }
 }
-list=list+fuck[i];
-if(i!=(fuck.length-1)){
-list=list+"\n";
-}
-}
-document.frm.sockslist.value=list;
+  document.frm.sockslist.value=list;
 }
 else{
 document.frm.sockslist.value="";
@@ -22,4 +22,8 @@ document.frm.sockslist.value="";
 
 
 
+}
+
+function isprox(inpx){
+if(inpx[0].split(":").lenght == 2){ retrn true}else{ return false }
 }
