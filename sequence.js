7d9f78be6a7d1f1283ded90044e5e0484be12e01
yaderson <39221701+yaderson@users.noreply.github.com>
2019-10-29 15:57:39 -0500

    add ranges

---
+++ sequence.js
@@ -0,0 +1,17 @@
+/*******sequence*******/
+function pad(d) {
+    return (d < 10) ? '0' + d.toString() : d.toString();
+}
+console.log('load...');
+let rangoi = 1;
+let rangom = 55
+
+let init =  1
+var todas = [];
+
+let bin = "37774174204";
+for (var i = 1; i <= 1;i++){
+	for(var x =1; x <= 99; x++){
+		console.log(bin+pad(i)+pad(x));
+	}
+}
